90d296390e4fada6061f35a1a9facdc611e753d7
antonio-1983 <antoniocastro2024@gmail.com>
2024-07-17 22:13:22 -0300

    iniciando el repo

---
+++ proyecto_portfolio/portfoliosprinboot/src/main/java/com/antonio/portfoliosprinboot/PortfoliosprinbootApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.portfoliosprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliosprinbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliosprinbootApplication.class, args);
+	}
+
+}
---
+++ proyecto_portfolio/portfoliosprinboot/src/test/java/com/antonio/portfoliosprinboot/PortfoliosprinbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.portfoliosprinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliosprinbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
